dc832c075feba94b45f6381dba3e04af85b3a56e
jeremygholland <jeremygholland@gmail.com>
2015-12-20 11:25:15 -0600

    added n(th) child styling

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
       $('.slider').slider(
-      	{full_width: false, height: 600, indicators: false, interval: 5000, transition: 500}
+      	{full_width: false, height: 600, indicators: true, interval: 5000, transition: 500}
       	);
     });
