cd0d99a5363cb77b8f82200dd22d07a129387dc2
jeremygholland <jeremygholland@gmail.com>
2015-12-20 12:29:33 -0600

    first draft complege, included music

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
+	$('.fa-volume-off').hide();
       $('.slider').slider(
-      	{full_width: false, height: 600, indicators: true, interval: 5000, transition: 500}
+      	{height: 600, indicators: false, interval: 6000, transition: 500}
       	);
+      $('.fa-volume-off').click(function(){
+      	$('audio').prop('muted', false);
+      	$('.fa-volume-off').hide();
+      	$('.fa-volume-up').show();
+      })
+      $('.fa-volume-up').click(function(){
+      	$('audio').prop('muted', true);
+      	$('.fa-volume-up').hide();
+      	$('.fa-volume-off').show();
+      })
     });
